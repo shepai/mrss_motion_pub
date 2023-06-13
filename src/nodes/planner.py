0e41cc6756522953010fb6f646288686ba34f47a
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-06-13 21:36:57 +0100

    Add files via upload

--- src/nodes/planner.py
+++ src/nodes/planner.py
@@ -33,22 +33,31 @@ class Planner:
         self.map = json.loads(msg.data)
 
         # TODO BEGIN MRSS: Use map for planning
-        GOAL=self.map['/goal'] #gives x and y
+        GOAL1=self.map['/goal'] #gives x and y
+        #GOAL2=self.map['/goal2'] # assuming goal 2 is the side gives x and y
+        #get angle between
+        #angle=math.atan(GOAL1[1]/GOAL1[0])
         #s=d/t
-        time=10
-        self.cmd.linear.x=GOAL[0]/time
-        self.cmd.linear.y=GOAL[1]/time
-        self.cmd.angular.z = 0.
+        #time=10
+        #self.cmd.linear.x=GOAL1[0]/time
+        #self.cmd.linear.y=GOAL1[1]/time
+        
         # END MRSS
 
         # Twist
         self.cmd = geometry_msgs.msg.Twist()
-
-        # TODO BEGIN MRSS: Update the current command
-        if GOAL[0]<0.1:
+        ang_speed=0
+        if GOAL1[1]<-0.1:
+            self.cmd.angular.z=0.15
+        elif GOAL1[1]>0.1:
+            self.cmd.angular.z=-0.15
+        elif GOAL1[0]>0.1:
+            self.cmd.linear.x = 0.1
+        else:
             self.cmd.linear.x = 0.
             self.cmd.linear.y = 0.
-            self.cmd.angular.z = 0.
+            self.cmd.angular.z=0
+        
         # END MRSS
 
     def spin(self):
