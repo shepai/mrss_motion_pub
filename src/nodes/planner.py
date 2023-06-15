1b95fded17cbda81856beba6ce23f1cb9a929fad
alicelb98 <43522262+alicelb98@users.noreply.github.com>
2023-06-15 14:09:46 -0400

    Add files via upload

--- src/nodes/planner.py
+++ src/nodes/planner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Reads the map output (see map_broadcaster.py) and publishes twist commands to reach the goal
 
+from curses import def_prog_mode
 import numpy as np
 import rospy
 from std_msgs.msg import String
@@ -33,47 +34,50 @@ class Planner:
         self.map = json.loads(msg.data)
 
         # TODO BEGIN MRSS: Use map for planning
-        GOAL1=self.map['/goal']
-        BOARD0=self.map['/board0']
-        BOARD1=self.map['/board1']
-        BOARD2=self.map['/board2']
-        BOARD3=self.map['/board3']
+        GOAL1 = self.map['/goal']
+        BOARD0 = self.map['/board0']
+        BOARD1 = self.map['/board1']
+        BOARD2 = self.map['/board2']
+        BOARD3 = self.map['/board3']
         if self.map.get('/obstacle1',None)!=None:
             coords_1=self.map['/obstacle1']
         if self.map.get('/obstacle2',None)!=None:
             coords_2=self.map['/obstacle2']
         self.cmd = geometry_msgs.msg.Twist()
-        if GOAL1[0]>0.1:
-            if GOAL1[1]<-0.1:
-                self.cmd.angular.z=-0.3
-            elif GOAL1[1]>0.1:
-                self.cmd.angular.z=0.3
-            else:
-                if (self.map.get('/obstacle1',None)!=None):
-                    if (coords_1[0]>0) and (-0.1<coords_1[1]<0.1):
-                        idx_max_dist_y = np.argmax([abs(BOARD0[1]),abs(BOARD1[1]),abs(BOARD2[1]),abs(BOARD3[1])]) 
-                        if idx_max_dist_y == 0:
-                            self.cmd.linear.y=0.2*(BOARD0[1]/abs(BOARD0[1]))
-                        elif idx_max_dist_y == 1:
-                            self.cmd.linear.y=0.2*(BOARD1[1]/abs(BOARD1[1]))
-                        elif idx_max_dist_y == 2:
-                            self.cmd.linear.y=0.2*(BOARD2[1]/abs(BOARD2[1]))
-                        else:
-                            self.cmd.linear.y=0.2*(BOARD3[1]/abs(BOARD3[1]))
-                    else:
-                        self.cmd.linear.x=0.2
-                
-                else:
-                    self.cmd.linear.x=0.2
-        elif GOAL1[0]<0.1:
+        if GOAL1[1]<-0.1: #If y distance is more than 10 cm turn the robot until it isn't
+            self.cmd.angular.z=-0.3      
+        elif GOAL1[1]>0.1:#If y distance is more than 10 cm turn the robot until it isn't
+            self.cmd.angular.z=0.3
+        elif GOAL1[0]<-0.1: #If the goal is behind you, turn. 
             if GOAL1[1]<0.:
-                self.cmd.angular.z=-0.3
-            else:
+                self.cmd.angular.z=-0.3      
+            elif GOAL1[1]>0.:
                 self.cmd.angular.z=0.3
         else:
-            self.cmd.linear.x=0.
-            self.cmd.linear.y=0.
-            self.cmd.angular.z=0.
+            if (-0.1<GOAL1[0]) and (GOAL1[0]<0.1): #if you are near the goal stop moving.
+                self.cmd.linear.x=0.
+                self.cmd.linear.y=0.
+                self.cmd.angular.z=0.
+
+            elif (self.map.get('/obstacle1',None)!=None) and (coords_1[0]>0) and (-0.1<coords_1[1]<0.1): #if you see an obstacle, move to the right or to the left, depending on where the farest board is (so the robot doesn't go out of bound)
+                idx_max_dist_y = np.argmax([abs(BOARD0[1]),abs(BOARD1[1]),abs(BOARD2[1]),abs(BOARD3[1])]) 
+                if idx_max_dist_y == 0:
+                    self.cmd.linear.y=0.2*(BOARD0[1]/abs(BOARD0[1]))
+                elif idx_max_dist_y == 1:
+                    self.cmd.linear.y=0.2*(BOARD1[1]/abs(BOARD1[1]))
+                elif idx_max_dist_y == 2:
+                    self.cmd.linear.y=0.2*(BOARD2[1]/abs(BOARD2[1]))
+                else:
+                    self.cmd.linear.y=0.2*(BOARD3[1]/abs(BOARD3[1]))
+                           
+            elif GOAL1[0]>0.1: #if the obstacle is in front of you , move forward
+                self.cmd.linear.x=0.2
+            else: #In case there are some cases i did not think about. 
+                self.cmd.linear.x=0.
+                self.cmd.linear.y=0.
+                self.cmd.angular.z=0.
+
+
         # END MRSS
 
     def spin(self):
